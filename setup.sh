5ef95e3f88b2d55d32ec5c6688c1ab8cd5ca3b7d
marc barry <67782240+enclave-marc-barry@users.noreply.github.com>
2021-08-09 13:29:34 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -157,6 +157,7 @@ Description=Enclave
 After=network.target
 
 [Service]
+Environment="DOTNET_BUNDLE_EXTRACT_BASE_DIR=/var/tmp/.net/enclave"
 ExecStart=/usr/bin/enclave supervisor-service
 
 [Install]
